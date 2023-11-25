c5b7eb822a3a2da2f68d38db86395b5358d3c854
Hiran Kavindu Vishmith <97301123+Hirankavindu@users.noreply.github.com>
2023-11-25 22:03:58 +0530

    create logo and search button and toggel button

---
+++ Movie_App/theme/index.js
@@ -0,0 +1,9 @@
+export const theme ={
+    background: '#eab308',
+    Text: '#eab308',
+    
+}
+export const styles = {
+    text: {color: theme.Text},
+    background: {backgroundColor: theme.background }
+}
