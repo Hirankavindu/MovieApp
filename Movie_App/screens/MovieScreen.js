93d957178abfa2d812db4af7f582cf2ccee356cc
Hiran Kavindu Vishmith <97301123+Hirankavindu@users.noreply.github.com>
2023-11-27 02:25:05 +0530

    create cast screen and movie Screen

--- Movie_App/screens/MovieScreen.js
+++ Movie_App/screens/MovieScreen.js
@@ -1,38 +1,72 @@
 import {
   View,
   Text,
-  ScrollView,
-  TouchableOpacity,
-  Dimensions,
-  Platform,
   Image,
+  Dimensions,
+  TouchableOpacity,
+  ScrollView,
+  Platform,
 } from "react-native";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useNavigation, useRoute } from "@react-navigation/native";
-import { SafeAreaView } from "react-native";
-import { ChevronLeftIcon } from "react-native-heroicons/outline";
-import { HeartIcon } from "react-native-heroicons/solid";
-import { styles, theme } from "../theme";
 import { LinearGradient } from "expo-linear-gradient";
+import { ArrowLeftIcon, ChevronLeftIcon } from "react-native-heroicons/outline";
+import { HeartIcon } from "react-native-heroicons/solid";
+import { SafeAreaView } from "react-native-safe-area-context";
 import Cast from "../components/cast";
 import MovieList from "../components/movieList";
+import {
+  fallbackMoviePoster,
+  fetchMovieCredits,
+  fetchMovieDetails,
+  fetchSimilarMovies,
+  image500,
+} from "../api/moviedb";
+import { styles, theme } from "../theme";
 import Loading from "../components/loading";
 
+const ios = Platform.OS == "ios";
+const topMargin = ios ? "" : " mt-3";
 var { width, height } = Dimensions.get("window");
-const ios = Platform.OS === "ios";
-const topMargin = ios ? "" : "mt-3";
 
 export default function MovieScreen() {
   const { params: item } = useRoute();
-  const [isFavourite, toggleFavourite] = useState(false);
   const navigation = useNavigation();
-  const [cast, setCast] = useState([1, 2, 3, 4, 5]);
-  const [similarMovies, setSimilarMovies] = useState([1, 2, 3, 4, 5]);
+  const [movie, setMovie] = useState({});
+  const [cast, setCast] = useState([]);
+  const [similarMovies, setSimilarMovies] = useState([]);
+  const [isFavourite, toggleFavourite] = useState(false);
   const [loading, setLoading] = useState(false);
-  let movieName = "Ant-Man and the Wasp: Quantumania";
+
   useEffect(() => {
-    //call the movie details api
-  });
+    setLoading(true);
+    getMovieDetials(item.id);
+    getMovieCredits(item.id);
+    getSimilarMovies(item.id);
+  }, [item]);
+
+  const getMovieDetials = async (id) => {
+    const data = await fetchMovieDetails(id);
+    console.log("got movie details");
+    setLoading(false);
+    if (data) {
+      setMovie({ ...movie, ...data });
+    }
+  };
+  const getMovieCredits = async (id) => {
+    const data = await fetchMovieCredits(id);
+    console.log("got movie credits");
+    if (data && data.cast) {
+      setCast(data.cast);
+    }
+  };
+  const getSimilarMovies = async (id) => {
+    const data = await fetchSimilarMovies(id);
+    console.log("got similar movies");
+    if (data && data.results) {
+      setSimilarMovies(data.results);
+    }
+  };
   return (
     <ScrollView
       contentContainerStyle={{ paddingBottom: 20 }}
@@ -42,38 +76,42 @@ export default function MovieScreen() {
       <View className="w-full">
         <SafeAreaView
           className={
-            "absolute z-20 w-full flex-row justify-between items-center px-4" +
+            "absolute z-20 w-full flex-row justify-between items-center px-4 " +
             topMargin
           }
         >
           <TouchableOpacity
-            onPress={() => navigation.goBack()}
             style={styles.background}
-            className="rounded-xl p-1 ml-3"
+            className="rounded-xl p-1"
+            onPress={() => navigation.goBack()}
           >
             <ChevronLeftIcon size="28" strokeWidth={2.5} color="white" />
           </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => toggleFavourite(!isFavourite)}
-            className="mr-3"
-          >
+
+          <TouchableOpacity onPress={() => toggleFavourite(!isFavourite)}>
             <HeartIcon
               size="35"
               color={isFavourite ? theme.background : "white"}
             />
           </TouchableOpacity>
         </SafeAreaView>
-
         {loading ? (
           <Loading />
         ) : (
           <View>
             <Image
-              source={require("../assets/images/moviePoster1.png")}
+              // source={require('../assets/images/moviePoster2.png')}
+              source={{
+                uri: image500(movie.poster_path) || fallbackMoviePoster,
+              }}
               style={{ width, height: height * 0.55 }}
             />
             <LinearGradient
-              colors={["transparent", "rgba(23,23,23,0.8)", "rgba(23,23,23,1)"]}
+              colors={[
+                "transparent",
+                "rgba(23, 23, 23, 0.8)",
+                "rgba(23, 23, 23, 1)",
+              ]}
               style={{ width, height: height * 0.4 }}
               start={{ x: 0.5, y: 0 }}
               end={{ x: 0.5, y: 1 }}
@@ -83,47 +121,56 @@ export default function MovieScreen() {
         )}
       </View>
 
-      {/* Movie details */}
+      {/* movie details */}
+
       <View style={{ marginTop: -(height * 0.09) }} className="space-y-3">
-        {/* Title */}
-        <Text className="text-white text-center text-3xl font-bold tracking-wider">
-          {movieName}
-        </Text>
-        {/* Status, release, runtime */}
-        <Text className="text-neutral-400 font-semibold text-base text-center">
-          Released . 2020 . 170 min
+        {/* title */}
+        <Text className="text-white text-center text-3xl font-bold tracking-widest">
+          {movie?.title}
         </Text>
 
-        {/* genres */}
+        {/* status, release year, runtime */}
+        {movie?.id ? (
+          <Text className="text-neutral-400 font-semibold text-base text-center">
+            {movie?.status} • {movie?.release_date?.split("-")[0] || "N/A"} •{" "}
+            {movie?.runtime} min
+          </Text>
+        ) : null}
+
+        {/* genres  */}
         <View className="flex-row justify-center mx-4 space-x-2">
-          <Text className="text-neutral-400 font-semibold text-base text-center">
-            Action .
-          </Text>
-          <Text className="text-neutral-400 font-semibold text-base text-center">
-            Thrill .
-          </Text>
-          <Text className="text-neutral-400 font-semibold text-base text-center">
-            Comedy .
-          </Text>
+          {movie?.genres?.map((genre, index) => {
+            let showDot = index + 1 != movie.genres.length;
+            return (
+              <Text
+                key={index}
+                className="text-neutral-400 font-semibold text-base text-center"
+              >
+                {genre?.name} {showDot ? "•" : null}
+              </Text>
+            );
+          })}
         </View>
-        {/* Description */}
+
+        {/* description */}
         <Text className="text-neutral-400 mx-4 tracking-wide">
-          A film, also called a movie or a motion picture, is a series of still
-          photographs on film projected onto a screen using light in rapid
-          succession. The optical phenomenon known as persistence of vision
-          gives the illusion of actual, smooth, and continuous movement.
+          {movie?.overview}
         </Text>
       </View>
 
       {/* cast */}
-      <Cast navigation={navigation} cast={cast} />
+      {movie?.id && cast.length > 0 && (
+        <Cast navigation={navigation} cast={cast} />
+      )}
 
-      {/* similar movies */}
-      {/* <MovieList
-        title="Similar Movies"
-        hideSeeAll={true}
-        data={similarMovies}
-      /> */}
+      {/* similar movies section */}
+      {movie?.id && similarMovies.length > 0 && (
+        <MovieList
+          title={"Similar Movies"}
+          hideSeeAll={true}
+          data={similarMovies}
+        />
+      )}
     </ScrollView>
   );
 }
